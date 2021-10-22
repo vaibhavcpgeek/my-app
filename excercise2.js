990f5f88d0608790ac1bdecbe7751ced06846e6e
Vaibhav Gupta <vaigupta4@publicisgroupe.net>
2021-10-22 11:31:53 +0100

    comments

--- excercise2.js
+++ excercise2.js
@@ -1,8 +1,10 @@
 export const once = (func) => {
   let result;
+  // Closure. It wil return function with locally saved func (i.e. callback)
   return (...args) => {
     if (func) {
       result = func.apply(this, args);
+      // Keep it null so that it won't run in successive calls
       func = null;
     }
     return result;
